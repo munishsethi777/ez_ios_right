45ba656dfee8f0b2bd8918f93ca307932da40840
munishsethi777 <munishsethi777@gmail.com>
2017-12-23 13:20:36 +0530

    staged

---
+++ ezae/testtabs/testtabs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testtabs
+//
+//  Created by Munish Sethi on 23/12/17.
+//  Copyright © 2017 Munish Sethi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

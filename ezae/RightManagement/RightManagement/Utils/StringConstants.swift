53a4a8be1b1570cfdf559be9b67c16f96781a255
munishsethi777 <munishsethi777@gmail.com>
2018-01-06 11:04:56 +0530

    changes

--- ezae/RightManagement/RightManagement/Utils/StringConstants.swift
+++ ezae/RightManagement/RightManagement/Utils/StringConstants.swift
@@ -34,6 +34,7 @@ struct StringConstants {
     static let GET_ACHIEVEMENTS = ACTION_API_URL + "UserAction.php?call=myAchievements&userSeq={0}&companyseq={1}"
     static let SAVE_NOTES = ACTION_API_URL + "NoteAction.php?call=saveNote&userSeq={0}&companySeq={1}&seq={2}&details={3}"
     static let DELETE_NOTES = ACTION_API_URL + "NoteAction.php?call=deleteNote&userSeq={0}&companySeq={1}&noteSeq={2}"
+    static let GET_USER_DETAIL = ACTION_API_URL + "UserAction.php?call=getUserDetail&userSeq={0}&companySeq={1}";
     
     //Learning Plan Actions
     static let GET_LEARNING_PLAN = ACTION_API_URL + "LearningPlanAction.php?call=getLearningPlans&userSeq={0}&companySeq={1}"
@@ -55,6 +56,9 @@ struct StringConstants {
     
     //ActivityAction
     static let SAVE_ACTIVITY = ACTION_API_URL + "ActivityAction.php?call=saveActivity&userSeq={0}&companySeq={1}&activityData={2}"
+    
+    
+    
     
     //Action Names
     static let DASHBOARD_DATA_ACTION = "getDashboardData"

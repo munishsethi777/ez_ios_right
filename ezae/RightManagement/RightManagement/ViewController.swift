2a7b12229ae4f74427cd3c800d7e7b92ef3e4e96
munishsethi777 <munishsethi777@gmail.com>
2018-01-10 11:12:48 +0530

    login window hide keyboard

--- ezae/RightManagement/RightManagement/ViewController.swift
+++ ezae/RightManagement/RightManagement/ViewController.swift
@@ -30,7 +30,9 @@ class ViewController: UIViewController {
             self.performSegue(withIdentifier: "DashboardTabController", sender: nil)
         }
     }
-    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.

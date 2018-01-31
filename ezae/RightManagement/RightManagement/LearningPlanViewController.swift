a0e6394f1096ccfbdd347d1c30dee8fea5652933
munishsethi777 <munishsethi777@gmail.com>
2018-01-31 09:48:47 +0530

    changes

--- ezae/RightManagement/RightManagement/LearningPlanViewController.swift
+++ ezae/RightManagement/RightManagement/LearningPlanViewController.swift
@@ -108,7 +108,7 @@ class LearningPlanViewController: UIViewController,UITableViewDataSource,UITable
             var  x = 165
             for var i in 0..<badges!.count{
                 let imageView = UIImageView.init()
-                imageView.frame = CGRect(x:x,y:64,width:22,height:22)
+                imageView.frame = CGRect(x:x,y:54,width:22,height:22)
                 let badgesJson = badges![i] as! [String: Any]
                 let badgeSeq = badgesJson["seq"] as! String
                 let imagePath = badgesJson["imagepath"] as! String

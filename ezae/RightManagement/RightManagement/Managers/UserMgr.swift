c7ea68c198017da0e60d9a46fc47b27666c4a8d3
bgaheer <baljeetgaheer@gmail.com>
2018-03-23 12:58:17 +0530

    changes

--- ezae/RightManagement/RightManagement/Managers/UserMgr.swift
+++ ezae/RightManagement/RightManagement/Managers/UserMgr.swift
@@ -37,6 +37,46 @@ class UserMgr{
         }
     }
     
+    func updateUser(response: [String: Any]){
+        let userSeq = PreferencesUtil.sharedInstance.getLoggedInUserSeq()
+        let fetchRequest:NSFetchRequest<NSFetchRequestResult> = NSFetchRequest.init(entityName: "Users")
+        fetchRequest.predicate = NSPredicate(format: "userseq == %d", userSeq)
+        do
+        {
+            let fetchedUsers = try coreDataManager.managedObjectContext.fetch(fetchRequest) as? [Users]
+            if fetchedUsers?.count == 1
+            {
+                let userJson = response["user"] as! [String:Any]
+                var user = fetchedUsers![0]
+                let userName = userJson["username"] as? String
+                let userSeq = Int(userJson["id"] as! String)! ;
+                let existingUser = getUserByUserSeq(userSeq: userSeq) as Users?
+                if(existingUser != nil){
+                    user = existingUser!;
+                }
+                user.username = userName
+                user.fullname = userJson["fullName"] as? String
+                user.userseq = Int16(userJson["id"] as! String)!
+                user.companyseq = Int16(userJson["companyseq"] as! String)!
+                user.emailid = userJson["email"] as? String
+                user.imagename = userJson["userImage"] as? String
+                user.profiles = userJson["profiles"] as? String
+                user.ismanager = false
+                do{
+                    try coreDataManager.managedObjectContext.save()
+                }
+                catch
+                {
+                    print(error)
+                }
+            }
+        }
+        catch
+        {
+            print(error)
+        }
+    }
+    
     func getUserByUserSeq(userSeq: Int)->Users?{
         let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Users")
         let id = String(userSeq)

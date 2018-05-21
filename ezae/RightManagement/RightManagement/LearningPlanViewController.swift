5fd6f462bf3e114dc866bffe7565fff8e5134235
bgaheer <baljeetgaheer@gmail.com>
2018-05-21 17:31:59 +0530

    chnage

--- ezae/RightManagement/RightManagement/LearningPlanViewController.swift
+++ ezae/RightManagement/RightManagement/LearningPlanViewController.swift
@@ -81,127 +81,130 @@ class LearningPlanViewController: UIViewController,UITableViewDataSource,UITable
         let score = learningPlan["score"] as! Int
         let badges = learningPlan["badges"] as? [Any]
         let moduleArr = learningPlan["modules"] as! [Any]
-        let moduleJson = moduleArr[0] as! [String: Any]
-        var colorArr:[UIColor] = []
-        var continueLabelText = "Continue"
-        var moduleImageUrl = moduleJson["imagepath"] as? String
-        if(moduleImageUrl == nil || (moduleImageUrl?.isEmpty)!){
-            moduleImageUrl = "dummy.jpg"
-        }
-        cell?.titleLabel.text = learningPlan["title"] as? String
-        cell?.desLabel.text = learningPlan["description"] as? String
-        let percent = learningPlan["percentCompleted"] as! Float
-        
-        let totalModule = moduleArr.count
-        
-        if(Int(percent) > 0){
-            cell?.progress.progress = percent / 100
-        }
-        if (self.cache.object(forKey: id as AnyObject) != nil){
-            cell?.lpImageView?.image = self.cache.object(forKey: id as AnyObject) as? UIImage
-            cell?.lpImageView.layer.cornerRadius = (cell?.lpImageView.frame.height)! / 2
-            cell?.lpImageView.clipsToBounds = true
-        }else {
-            moduleImageUrl = StringConstants.IMAGE_URL + "modules/" + moduleImageUrl!
-            if let url = NSURL(string: moduleImageUrl!) {
-                DispatchQueue.global().async {
-                    if let data = NSData(contentsOf: url as URL) {
-                        DispatchQueue.main.async {
-                            let img = UIImage(data: data as Data)
-                            cell?.lpImageView.image = UIImage(data: data as Data)
-                            cell?.lpImageView.layer.cornerRadius = (cell?.lpImageView.frame.height)! / 2
-                            cell?.lpImageView.clipsToBounds = true
-                            self.cache.setObject(img!, forKey: id as AnyObject)
+      
+        if moduleArr.count > 0 {
+            let moduleJson = moduleArr[0] as? [String: Any]
+            var colorArr:[UIColor] = []
+            var continueLabelText = "Continue"
+            var moduleImageUrl = moduleJson!["imagepath"] as? String
+            if(moduleImageUrl == nil || (moduleImageUrl?.isEmpty)!){
+                moduleImageUrl = "dummy.jpg"
+            }
+            cell?.titleLabel.text = learningPlan["title"] as? String
+            cell?.desLabel.text = learningPlan["description"] as? String
+            let percent = learningPlan["percentCompleted"] as! Float
+            
+            let totalModule = moduleArr.count
+            
+            if(Int(percent) > 0){
+                cell?.progress.progress = percent / 100
+            }
+            if (self.cache.object(forKey: id as AnyObject) != nil){
+                cell?.lpImageView?.image = self.cache.object(forKey: id as AnyObject) as? UIImage
+                cell?.lpImageView.layer.cornerRadius = (cell?.lpImageView.frame.height)! / 2
+                cell?.lpImageView.clipsToBounds = true
+            }else {
+                moduleImageUrl = StringConstants.IMAGE_URL + "modules/" + moduleImageUrl!
+                if let url = NSURL(string: moduleImageUrl!) {
+                    DispatchQueue.global().async {
+                        if let data = NSData(contentsOf: url as URL) {
+                            DispatchQueue.main.async {
+                                let img = UIImage(data: data as Data)
+                                cell?.lpImageView.image = UIImage(data: data as Data)
+                                cell?.lpImageView.layer.cornerRadius = (cell?.lpImageView.frame.height)! / 2
+                                cell?.lpImageView.clipsToBounds = true
+                                self.cache.setObject(img!, forKey: id as AnyObject)
+                            }
                         }
                     }
                 }
+                
+            }
+            cell?.scoreLabel.isHidden = true
+            cell?.pointLabel.isHidden = true
+            cell?.scoreValueLabel.isHidden = true
+            cell?.pointValueLabel.isHidden = true
+            cell?.percentLabel.text = String(Int(percent)) + "%"
+            cell?.launchPlanImageView.image = UIImage(named: "arrow_green.png")
+            cell?.bottomView.gradientColor = [UIColor.white.cgColor, UIColor.init(red: 110/255.0, green: 143/255.0, blue: 130/255.0, alpha: 1).cgColor]
+            if(completedModulesCount == 0){
+                cell?.bottomView.gradientColor = [UIColor.white.cgColor, UIColor.init(red: 99/255.0, green: 144/255.0, blue: 198/255.0, alpha: 1).cgColor]
+                continueLabelText = "Launch"
+                cell?.desLabel.isHidden = false
+                cell?.launchPlanImageView.image = UIImage(named: "arrow_up.png")
+            }else if(completedModulesCount == totalModule){
+                continueLabelText = "Review"
+                cell?.bottomView.gradientColor = [UIColor.white.cgColor, UIColor.init(red: 231/255.0, green: 124/255.0, blue: 34/255.0, alpha: 1).cgColor]
+                cell?.launchPlanImageView.image = UIImage(named: "arrow_orange.png")
+                let dateOfPlay = learningPlan["dateofplay"] as! String
+                let date = DateUtil.sharedInstance.stringToDate(dateStr: dateOfPlay)
+                cell?.percentLabel.text = DateUtil.sharedInstance.dateToString(date: date, format: DateUtil.format2)
+                cell?.scoreValueLabel.text = String(score)
+                cell?.pointValueLabel.text = String(points)
+                cell?.moduleCountLabel.isHidden = true
+                cell?.scoreLabel.isHidden = false
+                cell?.pointLabel.isHidden = false
+                cell?.scoreValueLabel.isHidden = false
+                cell?.pointValueLabel.isHidden = false
+                cell?.desLabel.isHidden = true
             }
             
-        }
-        cell?.scoreLabel.isHidden = true
-        cell?.pointLabel.isHidden = true
-        cell?.scoreValueLabel.isHidden = true
-        cell?.pointValueLabel.isHidden = true
-        cell?.percentLabel.text = String(Int(percent)) + "%"
-        cell?.launchPlanImageView.image = UIImage(named: "arrow_green.png")
-        cell?.bottomView.gradientColor = [UIColor.white.cgColor, UIColor.init(red: 110/255.0, green: 143/255.0, blue: 130/255.0, alpha: 1).cgColor]
-        if(completedModulesCount == 0){
-            cell?.bottomView.gradientColor = [UIColor.white.cgColor, UIColor.init(red: 99/255.0, green: 144/255.0, blue: 198/255.0, alpha: 1).cgColor]
-            continueLabelText = "Launch"
-            cell?.desLabel.isHidden = false
-            cell?.launchPlanImageView.image = UIImage(named: "arrow_up.png")
-        }else if(completedModulesCount == totalModule){
-            continueLabelText = "Review"
-            cell?.bottomView.gradientColor = [UIColor.white.cgColor, UIColor.init(red: 231/255.0, green: 124/255.0, blue: 34/255.0, alpha: 1).cgColor]
-            cell?.launchPlanImageView.image = UIImage(named: "arrow_orange.png")
-            let dateOfPlay = learningPlan["dateofplay"] as! String
-            let date = DateUtil.sharedInstance.stringToDate(dateStr: dateOfPlay)
-            cell?.percentLabel.text = DateUtil.sharedInstance.dateToString(date: date, format: DateUtil.format2)
-            cell?.scoreValueLabel.text = String(score)
-            cell?.pointValueLabel.text = String(points)
-            cell?.moduleCountLabel.isHidden = true
-            cell?.scoreLabel.isHidden = false
-            cell?.pointLabel.isHidden = false
-            cell?.scoreValueLabel.isHidden = false
-            cell?.pointValueLabel.isHidden = false
-            cell?.desLabel.isHidden = true
-        }
-        
-        if (completedModulesCount == 0 || completedModulesCount < totalModule){
-            cell?.moduleCountLabel.isHidden = false
-            var completedText = String(totalModule) + " Module"
-            if(totalModule > 1){
-                completedText = completedText + "s"
+            if (completedModulesCount == 0 || completedModulesCount < totalModule){
+                cell?.moduleCountLabel.isHidden = false
+                var completedText = String(totalModule) + " Module"
+                if(totalModule > 1){
+                    completedText = completedText + "s"
+                }
+                if(completedModulesCount > 0){
+                    completedText = String(completedModulesCount) + "/" + completedText
+                }
+                cell?.moduleCountLabel.text = completedText
             }
-            if(completedModulesCount > 0){
-                completedText = String(completedModulesCount) + "/" + completedText
+            cell?.continueLabel.text = continueLabelText
+            let theSubviews: Array = (cell?.contentView.subviews)!
+            for view in theSubviews{
+                if(view.tag == 5){
+                    view.removeFromSuperview()
+                }
             }
-            cell?.moduleCountLabel.text = completedText
-        }
-        cell?.continueLabel.text = continueLabelText
-        let theSubviews: Array = (cell?.contentView.subviews)!
-        for view in theSubviews{
-            if(view.tag == 5){
-                view.removeFromSuperview()
-            }
-        }
-        if(badges != nil){
-            var  x = 165
-            for var i in 0..<badges!.count{
-                let imageView = UIImageView.init()
-                imageView.frame = CGRect(x:x,y:55,width:22,height:22)
-                let badgesJson = badges![i] as! [String: Any]
-                let badgeSeq = badgesJson["seq"] as! String
-                let imagePath = badgesJson["imagepath"] as! String
-                if (self.cache.object(forKey: badgeSeq as AnyObject) != nil){
-                    imageView.image = self.cache.object(forKey: badgeSeq as AnyObject) as? UIImage
-                }else{
-                    let imageUrl = StringConstants.WEB_API_URL + imagePath
-                    if let url = NSURL(string: imageUrl) {
-                        if let data = NSData(contentsOf: url as URL) {
-                            let img = UIImage(data: data as Data)
-                            imageView.image = UIImage(data: data as Data)
-                            self.cache.setObject(img!, forKey: badgeSeq as AnyObject)
+            if(badges != nil){
+                var  x = 165
+                for var i in 0..<badges!.count{
+                    let imageView = UIImageView.init()
+                    imageView.frame = CGRect(x:x,y:55,width:22,height:22)
+                    let badgesJson = badges![i] as! [String: Any]
+                    let badgeSeq = badgesJson["seq"] as! String
+                    let imagePath = badgesJson["imagepath"] as! String
+                    if (self.cache.object(forKey: badgeSeq as AnyObject) != nil){
+                        imageView.image = self.cache.object(forKey: badgeSeq as AnyObject) as? UIImage
+                    }else{
+                        let imageUrl = StringConstants.WEB_API_URL + imagePath
+                        if let url = NSURL(string: imageUrl) {
+                            if let data = NSData(contentsOf: url as URL) {
+                                let img = UIImage(data: data as Data)
+                                imageView.image = UIImage(data: data as Data)
+                                self.cache.setObject(img!, forKey: badgeSeq as AnyObject)
+                            }
                         }
                     }
+                    imageView.tag = 5
+                    cell?.contentView.addSubview(imageView)
+                    x = x + 25
                 }
-                imageView.tag = 5
-                cell?.contentView.addSubview(imageView)
-                x = x + 25
             }
+            let tap = UITapGestureRecognizer(target: self, action: #selector(launch))
+            cell?.launchPlanImageView.tag = Int(id)!
+            cell?.launchPlanImageView.addGestureRecognizer(tap)
+            cell?.launchPlanImageView.isUserInteractionEnabled = true
+            cell?.progressView.sendSubview(toBack: (cell?.bottomView)!)
+            cell?.bottomView.layer.borderWidth = 0.3
+            cell?.bottomView.layer.borderColor = UIColor.lightGray.cgColor
+            cell?.bottomView.layer.shadowColor = UIColor.lightGray.cgColor
+            cell?.bottomView.layer.shadowOffset = CGSize(width: 1, height: 1)
+            cell?.bottomView.layer.shadowOpacity = 0.5
+            cell?.bottomView.layer.shadowRadius = 4.0
+            cell?.bottomView.commonInit()
         }
-        let tap = UITapGestureRecognizer(target: self, action: #selector(launch))
-        cell?.launchPlanImageView.tag = Int(id)!
-        cell?.launchPlanImageView.addGestureRecognizer(tap)
-        cell?.launchPlanImageView.isUserInteractionEnabled = true
-        cell?.progressView.sendSubview(toBack: (cell?.bottomView)!)
-        cell?.bottomView.layer.borderWidth = 0.3
-        cell?.bottomView.layer.borderColor = UIColor.lightGray.cgColor
-        cell?.bottomView.layer.shadowColor = UIColor.lightGray.cgColor
-        cell?.bottomView.layer.shadowOffset = CGSize(width: 1, height: 1)
-        cell?.bottomView.layer.shadowOpacity = 0.5
-        cell?.bottomView.layer.shadowRadius = 4.0
-        cell?.bottomView.commonInit()
         return cell!
     }
     
